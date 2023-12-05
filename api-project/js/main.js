10318664a703c99c41561fc2ae4c1d537c01d06e
Evan Gao <gaoevan37@gmail.com>
2023-12-05 09:25:02 -0500

    asdf

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init(); // npm install aos --save
+
+// filter github apis by popularity 
+// find seo based on website and which ones r most popular
+// https://medium.com/@luisprooc/free-awesome-apis-for-your-next-projects-9a31345cb114
+// https://rapidapi.com/guides/ten-api-projects
+// https://github.com/public-apis/public-apis

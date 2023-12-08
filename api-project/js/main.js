e6698735d40c6e87efa3d26cfc4f5b0894ca3d3f
Evan Gao <gaoevan37@gmail.com>
2023-12-08 09:25:27 -0500

    adsfasdfq23re

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -5,7 +5,7 @@ import "aos/dist/aos.css";
 
 AOS.init(); // npm install aos --save
 
-const URL = "https://developers.google.com/custom-search/v1/overview"
+const URL = "https://serpapi.com/search.json?q=?location=?engine=google&key=https://serpapi.com/manage-api-key=b58e348368dbc07d8245c2aafa4750df9263f46f502b219e6a798ad2a203e50c"
 
 async function getData(URL) {
     try{
@@ -23,8 +23,13 @@ getData(URL)
 
 // https://medium.com/@luisprooc/free-awesome-apis-for-your-next-projects-9a31345cb114
 // https://rapidapi.com/guides/ten-api-projects
+// https://apilist.fun/api/google-custom-search-api
 
 // search up keywords and get websites related to keyword
 // filter by criteria
 // get pic of website home page for each card div
 // yt search api and google job api give yt vids based off job
+
+//v2/pokemon?limit=151 (gets first 151 pokemons)
+// &offset=151 (gets pokemone after 151)
+//apikey?apikey=blahblah&page=2

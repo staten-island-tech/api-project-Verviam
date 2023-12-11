976bf835283eed067e26e9d8cddf86160c5eaeed
Verviam <gaoevan37@gmail.com>
2023-12-10 23:31:35 -0500

    aaaaaaaaa

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -3,9 +3,11 @@ import '../styles/style.css';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+
 AOS.init(); // npm install aos --save
 
-const URL = "https://serpapi.com/search.json?q=?location=?engine=google&key=https://serpapi.com/manage-api-key=b58e348368dbc07d8245c2aafa4750df9263f46f502b219e6a798ad2a203e50c"
+const URL = "https://api.spoonacular.com/food/ingredients/9266/information?amount=1?apiKey=f9fb379cedb74dcbb6c758ac6a3c7cef&includeNutrition=true"
+// https://spoonacular.com/food-api/docs#Authentication
 
 async function getData(URL) {
     try{
@@ -28,7 +30,11 @@ getData(URL)
 // search up keywords and get websites related to keyword
 // filter by criteria
 // get pic of website home page for each card div
-// yt search api and google job api give yt vids based off job
+// food recipe builder (have stats of certain foods and recipes, have another api for pics for final recipe)
+// maybe too lazy to cook option and then find restaurants with those specific foods in their menus (https://rapidapi.com/ptwebsolution/api/restaurants222/details)
+// combine with weather api or local restaurants api
+
+// random dad jokes just for viewer attention span https://apilist.fun/api/icanhazdadjoke
 
 //v2/pokemon?limit=151 (gets first 151 pokemons)
 // &offset=151 (gets pokemone after 151)
